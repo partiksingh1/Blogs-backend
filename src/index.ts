c96a48f4d856bde04eab584f58ae3fd2ab758db0
partiksingh1 <20btrcs105@jainuniversity.ac.in>
2025-02-12 02:00:05 +0530

    first commit

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { blogRouter } from "./routes/blogRoute.js";
 const port = 8000;
 const app = express();
 
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.get("/", (req,res)=>{
